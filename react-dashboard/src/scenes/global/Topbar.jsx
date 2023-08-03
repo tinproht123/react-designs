43601f0187c76c660ed03300bfe0cdfa4d8d2d94
Minh Anh <tinproht123@gmail.com>
2023-08-03 22:04:34 +0700

    first commit

---
+++ react-dashboard/src/scenes/global/Topbar.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return <div>Topbar</div>;
+};
+
+export default Topbar;
